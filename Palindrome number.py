d56db63c3456cd8ad0dbd53db68e0a34c9cfe7c7
Welberth Azevedo Barbosa <welberthab@gmail.com>
2023-05-24 18:32:15 -0300

    Solution

---
+++ Palindrome number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num = x
+        rev = 0
+        while x > 0:
+            rev = (rev * 10) + (x % 10)
+            x = x // 10
+        return num == rev
